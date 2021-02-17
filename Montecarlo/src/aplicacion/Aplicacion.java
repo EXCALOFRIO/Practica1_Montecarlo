7065ac161f81dfb9bf231777eff31043cd55acfe
Alejandro Ramirez <alejandro.ramirezlarena@usp.ceu.es>
2021-02-17 23:44:41 +0100

    2.0

--- Montecarlo/src/aplicacion/Aplicacion.java
+++ Montecarlo/src/aplicacion/Aplicacion.java
@@ -3,8 +3,8 @@ import mates.*;
 
 public class Aplicacion {
 	public static void main(String[] args){
-		//String entrada = args[0];
-		String entrada = "100000";
+		String entrada = args[0];
+		//String entrada = "100000";
 		int pasos=Integer.parseInt(entrada);
 		Matematicas.calcularPi(pasos);
 	}
