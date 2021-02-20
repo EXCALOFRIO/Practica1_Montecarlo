97fd9447177b0ab25444359bd577b23c00bc559c
Alejandro <Alejandro@DESKTOP-JQ102UM>
2021-02-20 14:08:18 +0100

    2.1

--- Montecarlo/src/aplicacion/Aplicacion.java
+++ Montecarlo/src/aplicacion/Aplicacion.java
@@ -3,9 +3,9 @@ import mates.*;
 
 public class Aplicacion {
 	public static void main(String[] args){
-		String entrada = args[0];
-		//String entrada = "100000";
-		int pasos=Integer.parseInt(entrada);
+		//String entrada = args[0];
+		String entrada = "1090";
+		Integer pasos = Integer.parseInt(entrada);
 		Matematicas.calcularPi(pasos);
 	}
 }
--- Montecarlo/src/mates/Matematicas.java
+++ Montecarlo/src/mates/Matematicas.java
@@ -2,34 +2,27 @@ package mates;
 
 public class Matematicas {
 	
-	public static void calcularPi(int pasos) {
-		double fuera = 0;
-		double dentro = 0;
-		double pi = 0;
-		
+	public static void calcularPi(Integer pasos) {
+		Double pi = 0.0;
+		Double dentro = 0.0;
+		Double fuera = 0.0;
+		Double porcentaje = 0.0;
 		for(int i =0 ; i<pasos ; i++) {
-			double x =  Math.random()*1;
-			double y =  Math.random()*1;
 			
-			double porcentaje =(pi/Math.PI)*100;
-			double porcentajeRed = Math.round(porcentaje*100.0)/100.0;
+			Double distancia = (Math.pow((Math.random()*1), 2)) + (Math.pow((Math.random()*1), 2));
 			
-			pi = (4*dentro)/(1*(fuera+dentro));
+			if (distancia<=1) dentro++;
+				else fuera++;
 			
-			System.out.println("Pi: " + pi + " ====> " + porcentajeRed + "%");
+			porcentaje = Math.round(((pi/Math.PI)*100)*100.0)/100.0;
+			Double total = dentro + fuera;
+			pi = (4*dentro)/(1*(total));
 			
-			if(x*x+y*y <= 1) {
-				
-				dentro += 1;
-				
-			}else {
-			
-				fuera += 1;
-			}
+			System.out.println("Pi: " + pi + " ====> " + porcentaje + "%");
 		}
-		double total = (fuera+dentro);
-		System.out.println( "\n" + "La aproximacion por el metodo de montecrlo con " + total + " dardos lanzados es de: " + pi);
-		System.out.println("El numero de dardos que entraron dentro es de: "+ dentro);
-		System.out.println("El numero de dardos que salieron del circulo es de: "+ fuera);
+		System.out.println("\n" + "La aproximacion por el metodo de montecrlo con " + pasos + " dardos lanzados es de: " + pi +
+				"\nUn " + porcentaje + "%" +
+				"\nEl numero de dardos que entraron dentro es de: "+ dentro +
+				"\nEl numero de dardos que salieron del circulo es de: "+ fuera);
 	}
 }
