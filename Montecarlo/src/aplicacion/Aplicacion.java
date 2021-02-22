b83a09495681d219c3bd69ab8f41d11a630eb9e9
Alejandro <Alejandro@DESKTOP-JQ102UM.mshome.net>
2021-02-22 12:15:18 +0100

    2.2

--- Montecarlo/src/aplicacion/Aplicacion.java
+++ Montecarlo/src/aplicacion/Aplicacion.java
@@ -1,11 +1,30 @@
+/*
+Copyright [2020] [Alejandro Ramirez Larena copyright]
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package aplicacion;
 import mates.*;
-
+/**
+ * Clase Aplicacion que llama a generarPi e introduce los pasos (numero de dardos)
+ * @author Alejandro Ramirez
+ * @version 2.2, 22/02/2021
+ */
 public class Aplicacion {
+	/**
+	 * Metodo que procesa la peticion de la sentencia
+	 *@param  args entrada que establece el numero de dardos
+	 */
 	public static void main(String[] args){
-		//String entrada = args[0];
-		String entrada = "1090";
-		Integer pasos = Integer.parseInt(entrada);
+		String entrada = args[0];
+		long pasos = Long.parseLong(entrada);
 		Matematicas.calcularPi(pasos);
 	}
 }
--- Montecarlo/src/mates/Matematicas.java
+++ Montecarlo/src/mates/Matematicas.java
@@ -1,12 +1,29 @@
+/*
+Copyright [2020] [Alejandro Ramirez Larena copyright]
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 package mates;
-
+/**
+ * Clase Matematicas que tiene la funcion generarPi para generar PI
+ * @author Alejandro Ramirez
+ * @version 2.2, 22/02/2021
+ */
 public class Matematicas {
-	
-	public static void calcularPi(Integer pasos) {
-		Double pi = 0.0;
-		Double dentro = 0.0;
-		Double fuera = 0.0;
-		Double porcentaje = 0.0;
+	/**
+	 * Metodo que calcula PI 
+	 *@param  pasos entrada que establece el numero de dardos
+	 */
+	public static void calcularPi(long pasos) {
+		Double pi = 0.0, dentro = 0.0, fuera = 0.0, porcentaje = 0.0;
+		
 		for(int i =0 ; i<pasos ; i++) {
 			
 			Double distancia = (Math.pow((Math.random()*1), 2)) + (Math.pow((Math.random()*1), 2));
@@ -14,11 +31,12 @@ public class Matematicas {
 			if (distancia<=1) dentro++;
 				else fuera++;
 			
+			//Uso Math.PI para calcular el porcentaje de acierto que tiene el codigo
 			porcentaje = Math.round(((pi/Math.PI)*100)*100.0)/100.0;
 			Double total = dentro + fuera;
 			pi = (4*dentro)/(1*(total));
 			
-			System.out.println("Pi: " + pi + " ====> " + porcentaje + "%");
+			//System.out.println("Pi: " + pi + " ====> " + porcentaje + "%");
 		}
 		System.out.println("\n" + "La aproximacion por el metodo de montecrlo con " + pasos + " dardos lanzados es de: " + pi +
 				"\nUn " + porcentaje + "%" +
